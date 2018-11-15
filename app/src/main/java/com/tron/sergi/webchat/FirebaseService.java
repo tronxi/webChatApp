b1e64ddf1d3c2583fe1172c22e201680150edf67
sergio garcia sanchez <sergio.gsanchez@alumnos.upm.es>
2018-11-15 03:25:06 +0100

    recibir notificaciones firebase

---
+++ app/src/main/java/com/tron/sergi/webchat/FirebaseService.java
@@ -0,0 +1,21 @@
+package com.tron.sergi.webchat;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+
+public class FirebaseService extends FirebaseMessagingService
+{
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.e("NEW_TOKEN",s);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+}
